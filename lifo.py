6491871fd7cbf8de6eb2cd620b75bc88e8ac80d9
JimwellMangulabnan <mangulabnanjimwell22@gmail.com>
2022-12-14 01:15:30 +0800

    importing Stack from queues3

--- lifo.py
+++ lifo.py
@@ -1,17 +1,2 @@
 from queues3 import Stack
 
-lifo = Stack("1st", "2nd", "3rd")
-for element in lifo:
-    print(element)
-
-lifo = []
-
-lifo.append("1st")
-lifo.append("2nd")
-lifo.append("3rd")
-
-print(lifo.pop())
-
-print(lifo.pop())
-
-print(lifo.pop())
