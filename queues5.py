4aa825d0c35eac8b8106f451a07b5f1b33b887cc
JimwellMangulabnan <mangulabnanjimwell22@gmail.com>
2022-12-14 02:34:43 +0800

    importing packages from collections and heapq, with class of priorityqueue but in negative

--- queues5.py
+++ queues5.py
@@ -1,25 +1,26 @@
-from heapq import heappop, heappush
 from collections import deque
-class PriorityQueue:
-    def __init__(self):
-        self._elements = []
+from heapq import heappop, heappush
 
-    def enqueue_with_priority(self, priority, value):
-        heappush(self._elements, (-priority, value))
+class Queue:
+    def __init__(self, *elements):
+        self._elements = deque(elements)
+
+    def __len__(self):
+        return len(self._elements)
+
+    def __iter__(self):
+        while len(self) > 0:
+            yield self.dequeue()
+
+    def enqueue(self, element):
+        self._elements.append(element)
 
     def dequeue(self):
-        return heappop(self._elements)[1]
+        return self._elements.popleft()
 
-messages = PriorityQueue()
-
-print(messages.dequeue())
-
-print(messages.dequeue())
-
-print(messages.dequeue())
-
-print(messages.dequeue())
-
+class Stack(Queue):
+    def dequeue(self):
+        return self._elements.pop()
 
 class PriorityQueue:
     def __init__(self):
