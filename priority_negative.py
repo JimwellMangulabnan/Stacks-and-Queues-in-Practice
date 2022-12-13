7662a1a62b368b33a9afed45411ee1741d878458
JimwellMangulabnan <mangulabnanjimwell22@gmail.com>
2022-12-14 02:40:46 +0800

    messages values

--- priority_negative.py
+++ priority_negative.py
@@ -3,3 +3,10 @@ from queues5 import PriorityQueue
 CRITICAL = 3
 IMPORTANT = 2
 NEUTRAL = 1
+
+messages = PriorityQueue()
+messages.enqueue_with_priority(IMPORTANT, "Windshield wipers turned on")
+messages.enqueue_with_priority(NEUTRAL, "Radio station tuned in")
+messages.enqueue_with_priority(CRITICAL, "Brake pedal depressed")
+messages.enqueue_with_priority(IMPORTANT, "Hazard lights turned on")
+
