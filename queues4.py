78ffc31ce25ab7666dcf79a7940aa78e178a9fe8
JimwellMangulabnan <mangulabnanjimwell22@gmail.com>
2022-12-14 01:48:42 +0800

    importing packages from collections and heapq

--- queues4.py
+++ queues4.py
@@ -1,22 +1,3 @@
-from heapq import heappush
+from collections import deque
+from heapq import heappop, heappush
 
-fruits = []
-heappush(fruits, "orange")
-heappush(fruits, "apple")
-heappush(fruits, "banana")
-
-print(fruits)
-
-from heapq import heappop
-
-heappop(fruits)
-
-print(fruits)
-
-person1 = ("John", "Brown", 42)
-person2 = ("John", "Doe", 42)
-person3 = ("John", "Doe", 24)
-
-print(person1 < person2)
-
-print(person2 < person3)
