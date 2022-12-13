7bf569c6414d156f63cac377d9486763defb7542
JimwellMangulabnan <mangulabnanjimwell22@gmail.com>
2022-12-14 02:41:49 +0800

    displaying the value of messages as variable

--- priority_negative.py
+++ priority_negative.py
@@ -10,3 +10,7 @@ messages.enqueue_with_priority(NEUTRAL, "Radio station tuned in")
 messages.enqueue_with_priority(CRITICAL, "Brake pedal depressed")
 messages.enqueue_with_priority(IMPORTANT, "Hazard lights turned on")
 
+print(messages.dequeue())
+print(messages.dequeue())
+print(messages.dequeue())
+print(messages.dequeue())
