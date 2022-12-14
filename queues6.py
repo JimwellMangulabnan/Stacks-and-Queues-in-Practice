2889ee5625b3b2126e4365be0d6665e9477b21f8
JimwellMangulabnan <mangulabnanjimwell22@gmail.com>
2022-12-15 03:15:43 +0800

    updated priority queue class

--- queues6.py
+++ queues6.py
@@ -23,4 +23,14 @@ class Stack(Queue):
     def dequeue(self):
         return self._elements.pop()
 
+class PriorityQueue:
+    def __init__(self):
+        self._elements = []
+        self._counter = count()
 
+    def enqueue_with_priority(self, priority, value):
+        element = (-priority, next(self._counter), value)
+        heappush(self._elements, element)
+
+    def dequeue(self):
+        return heappop(self._elements)[-1]
