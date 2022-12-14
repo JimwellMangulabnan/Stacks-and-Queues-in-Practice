9279397c2a77003ee7d38542f8cbbecb6b29c79e
JimwellMangulabnan <mangulabnanjimwell22@gmail.com>
2022-12-15 03:25:40 +0800

    printing values from class

--- cornercase.py
+++ cornercase.py
@@ -14,3 +14,13 @@ class Message:
 wipers = Message("Windshield wipers turned on")
 hazard_lights = Message("Hazard lights turned on")
 
+print(wipers < hazard_lights)
+
+messages = PriorityQueue()
+messages.enqueue_with_priority(NEUTRAL, "Radio station tuned in")
+messages.enqueue_with_priority(CRITICAL, "Brake pedal depressed")
+messages.enqueue_with_priority(CRITICAL, wipers)
+messages.enqueue_with_priority(IMPORTANT, hazard_lights)
+
+
+print(messages.dequeue())
