5ceca6e27d245ba08ef3ca3f6114c121ad37e292
JimwellMangulabnan <mangulabnanjimwell22@gmail.com>
2022-12-15 03:35:25 +0800

    updating current queues for iterable mixin from queues6

--- mixinclass.py
+++ mixinclass.py
@@ -10,3 +10,28 @@ class IterableMixin:
         while len(self) > 0:
             yield self.dequeue()
 
+class Queue(IterableMixin):
+    def __init__(self, *elements):
+        self._elements = deque(elements)
+
+    def enqueue(self, element):
+        self._elements.append(element)
+
+    def dequeue(self):
+        return self._elements.popleft()
+
+class Stack(Queue):
+    def dequeue(self):
+        return self._elements.pop()
+
+class PriorityQueue(IterableMixin):
+    def __init__(self):
+        self._elements = []
+        self._counter = count()
+
+    def enqueue_with_priority(self, priority, value):
+        element = (-priority, next(self._counter), value)
+        heappush(self._elements, element)
+
+    def dequeue(self):
+        return heappop(self._elements)[-1]
