64ebc23c7eb9bc457b7fef5a36895c335c817c0f
JimwellMangulabnan <mangulabnanjimwell22@gmail.com>
2022-12-15 03:32:22 +0800

    creating another class for iterablemixin

--- mixinclass.py
+++ mixinclass.py
@@ -2,3 +2,11 @@ from collections import deque
 from heapq import heappop, heappush
 from itertools import count
 
+class IterableMixin:
+    def __len__(self):
+        return len(self._elements)
+
+    def __iter__(self):
+        while len(self) > 0:
+            yield self.dequeue()
+
