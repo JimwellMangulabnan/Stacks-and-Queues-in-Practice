95b718ef7a6d094aa6e91a38a09a89c567297336
JimwellMangulabnan <mangulabnanjimwell22@gmail.com>
2022-12-16 00:21:54 +0800

    importing networkx as nx and inputting nodes for london

---
+++ graph.py
@@ -0,0 +1,7 @@
+print("\n\t*********  PROGRAMMED BY  ********")
+print("\t***** JIMWELL L. MANGULABNAN *****")
+print("\t********** BSCOE 2-2 *************")
+print()
+
+import networkx as nx
+print(nx.nx_agraph.read_dot("roadmap.dot"))
