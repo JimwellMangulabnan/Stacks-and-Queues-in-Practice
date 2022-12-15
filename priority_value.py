f6cb789487bd50dc410f95ebd199b587e20a1dab
JimwellMangulabnan <mangulabnanjimwell22@gmail.com>
2022-12-15 23:51:15 +0800

    displaying the order of priority from smallest to highest value

---
+++ priority_value.py
@@ -0,0 +1,22 @@
+print("\n\t*********  PROGRAMMED BY  ********")
+print("\t***** JIMWELL L. MANGULABNAN *****")
+print("\t********** BSCOE 2-2 *************")
+print()
+
+from queues4 import PriorityQueue
+
+CRITICAL = 3
+IMPORTANT = 2
+NEUTRAL = 1
+
+messages = PriorityQueue()
+messages.enqueue_with_priority(IMPORTANT, "Windshield wipers turned on")
+messages.enqueue_with_priority(NEUTRAL, "Radio station tuned in")
+messages.enqueue_with_priority(CRITICAL, "Brake pedal depressed")
+messages.enqueue_with_priority(IMPORTANT, "Hazard lights turned on")
+
+print("The first priority is :",messages.dequeue())
+print("The second priority is :",messages.dequeue())
+print("The third priority is :",messages.dequeue())
+print("The fourth priority is :",messages.dequeue())
+print()
