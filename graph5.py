18e63e97c3c9d854544b8e22ab1cc75d1b770486
JimwellMangulabnan <mangulabnanjimwell22@gmail.com>
2022-12-16 00:51:19 +0800

    import the City, load_graph from graph4

---
+++ graph5.py
@@ -0,0 +1,19 @@
+print("\n\t*********  PROGRAMMED BY  ********")
+print("\t***** JIMWELL L. MANGULABNAN *****")
+print("\t********** BSCOE 2-2 *************")
+print()
+
+from graph import City, load_graph
+
+nodes, graph = load_graph("roadmap.dot", City.from_dict)
+
+print("/t/t", nodes["london"])
+
+
+
+
+
+
+
+
+print("/t/t", graph)
