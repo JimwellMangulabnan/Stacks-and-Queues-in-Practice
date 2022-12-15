8d457c60919dbced31aca413239ed5f0cbae888c
JimwellMangulabnan <mangulabnanjimwell22@gmail.com>
2022-12-16 01:09:49 +0800

    using for statement for graph neighbor and it will print outs the names of neighbor

---
+++ graph6.py
@@ -0,0 +1,7 @@
+print("\n\t*********  PROGRAMMED BY  ********")
+print("\t***** JIMWELL L. MANGULABNAN *****")
+print("\t********** BSCOE 2-2 *************")
+print()
+
+for neighbor in graph.neighbors(nodes["london"]):
+    print("/t/t", neighbor.name)
