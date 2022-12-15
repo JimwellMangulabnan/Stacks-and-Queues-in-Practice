706742406d44da0fff1951d787ebf91bec6e87ee
JimwellMangulabnan <mangulabnanjimwell22@gmail.com>
2022-12-16 01:14:23 +0800

    import the current program of load_graph and city

---
+++ graph7.py
@@ -0,0 +1,9 @@
+print("\n\t*********  PROGRAMMED BY  ********")
+print("\t***** JIMWELL L. MANGULABNAN *****")
+print("\t********** BSCOE 2-2 *************")
+print()
+
+from graph4 import City, load_graph
+
+nodes, graph = load_graph("roadmap.dot", City.from_dict)
+
