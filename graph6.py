f581b68e11c6c7e508d136446c5a93a8cbe6579e
JimwellMangulabnan <mangulabnanjimwell22@gmail.com>
2022-12-16 01:12:46 +0800

    importing the current City, load_graph from graph 4

--- graph6.py
+++ graph6.py
@@ -3,5 +3,9 @@ print("\t***** JIMWELL L. MANGULABNAN *****")
 print("\t********** BSCOE 2-2 *************")
 print()
 
+from graph4 import City, load_graph
+
+nodes, graph = load_graph("roadmap.dot", City.from_dict)
+
 for neighbor in graph.neighbors(nodes["london"]):
     print("/t/t", neighbor.name)
