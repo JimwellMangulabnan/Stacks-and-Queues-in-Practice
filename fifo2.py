0ae87de486fd9c570b6d63fbdbdf93da44ab29bc
JimwellMangulabnan <mangulabnanjimwell22@gmail.com>
2022-12-15 23:31:22 +0800

    Displaying the output for FIFO stacks

--- fifo2.py
+++ fifo2.py
@@ -1,10 +1,17 @@
+print("\n\t*********  PROGRAMMED BY  ********")
+print("\t***** JIMWELL L. MANGULABNAN *****")
+print("\t********** BSCOE 2-2 *************")
+print()
+
 from queues2 import Queue
 
 fifo = Queue("1st", "2nd", "3rd")
-print(len(fifo))
+print("\t     `Stack: 1st, 2nd, 3rd`\n")
+print("\t\tNos of stack: ", len(fifo))
 
 for element in fifo:
-    print(element)
+    print("\n\tThis is the order output using FIFO: \n\t\t\t", element)
 
 
-print(len(fifo))
+print("\n\tCurrent number of stack:", len(fifo))
+print()
