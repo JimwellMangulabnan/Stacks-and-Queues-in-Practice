cedc479d5fad02272897b63793b445ebd305ad33
JimwellMangulabnan <mangulabnanjimwell22@gmail.com>
2022-12-15 23:24:00 +0800

    printing the elements that dequeued using LIFO stacks

--- lifo.py
+++ lifo.py
@@ -1,20 +1,28 @@
+print("\n\t*********  PROGRAMMED BY  ********")
+print("\t***** JIMWELL L. MANGULABNAN *****")
+print("\t********** BSCOE 2-2 *************")
+print()
+
 from queues3 import Stack
 
-lifo = Stack("1st", "2nd", "3rd")
+lifo = Stack("`1st`", "`2nd`", "`3rd`")
 
-print("Nos of stack:", len(lifo))
+print("\t     `Stack: 1st, 2nd, 3rd`\n")
+print("\t\tNos of stack: ", len(lifo))
 
 for element in lifo:
-    print(element)
-
+    print("\n\tThis is the order output using LIFO: \n\t\t\t",element)
+print()
 lifo = []
 
 lifo.append("1st")
 lifo.append("2nd")
 lifo.append("3rd")
 
-print(lifo.pop())
+print("The following elements are the order output of LIFO stacks: \n")
+print("\t\t\t",lifo.pop())
 
-print(lifo.pop())
+print("\t\t\t",lifo.pop())
 
-print(lifo.pop())
+print("\t\t\t",lifo.pop())
+print()
