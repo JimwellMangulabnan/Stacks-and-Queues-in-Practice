a0f0554eca6ff824ce6962ab4cb1e1fbbac94560
JimwellMangulabnan <mangulabnanjimwell22@gmail.com>
2022-12-15 22:40:49 +0800

    displaying the names from resulting heap

---
+++ priority_heappush.py
@@ -0,0 +1,14 @@
+print("\n\t*********  PROGRAMMED BY  ********")
+print("\t***** JIMWELL L. MANGULABNAN *****")
+print("\t********** BSCOE 2-2 *************")
+print()
+
+from heapq import heappush
+
+fruits = []
+heappush(fruits, "orange")
+heappush(fruits, "apple")
+heappush(fruits, "banana")
+
+print("\tThe following Fruit names in the resulting heap are: \n\t\t",fruits)
+print()
