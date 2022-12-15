b494bd85e38eed5bd5576037c441ac95e406b148
JimwellMangulabnan <mangulabnanjimwell22@gmail.com>
2022-12-15 22:55:00 +0800

    stating the different condition if true or false via tuple comparison

---
+++ priority_tuple.py
@@ -0,0 +1,14 @@
+print("\n\t*********  PROGRAMMED BY  ********")
+print("\t***** JIMWELL L. MANGULABNAN *****")
+print("\t********** BSCOE 2-2 *************")
+print()
+
+person1 = ("Jimmy", "Lambrinto", 22)
+person2 = ("Jimwell", "Mangulabnan", 22)
+person3 = ("Jimwell", "Mangulabnan", 11)
+
+print("\tState if person 1 < person 2: ",person1 < person2)
+print("\n\tState if person 2 < person 3: ",person2 < person3)
+print("\n\tState if person 3 < person 1: ",person3 < person1)
+print("\n\tState if person 3 > person 1: ",person3 > person1)
+print()
