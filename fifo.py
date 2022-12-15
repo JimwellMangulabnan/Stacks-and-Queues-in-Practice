fd5c15510c27765509126e89eec2889320510e22
JimwellMangulabnan <mangulabnanjimwell22@gmail.com>
2022-12-15 23:35:35 +0800

    display the FIFO elements

--- fifo.py
+++ fifo.py
@@ -1,12 +1,17 @@
-from queues import Queue
+print("\n\t*********  PROGRAMMED BY  ********")
+print("\t***** JIMWELL L. MANGULABNAN *****")
+print("\t********** BSCOE 2-2 *************")
+print()
 
+from queues import Queue
+print("\t     `Stack: 1st, 2nd, 3rd`\n")
 fifo = Queue()
 fifo.enqueue("1st")
 fifo.enqueue("2nd")
 fifo.enqueue("3rd")
 
-print(fifo.dequeue())
-
-print(fifo.dequeue())
-
-print(fifo.dequeue())
+print("The following elements are the order output of FIFO stacks: \n")
+print("\t\t\t", fifo.dequeue())
+print("\t\t\t", fifo.dequeue())
+print("\t\t\t", fifo.dequeue())
+print()
